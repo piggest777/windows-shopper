b90ff0fcf5045d30443b146e28aaffed0de57054
Piggest <piggest@mail.ru>
2018-01-09 05:06:58 +0300

    CalcBtn

--- windows-shopper/windows-shopper/Controler/MainVC.swift
+++ windows-shopper/windows-shopper/Controler/MainVC.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.6518368445, blue: 0.1364178927, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+
+    }
+    
+    @objc func calculate () {
+        print("we got here")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
